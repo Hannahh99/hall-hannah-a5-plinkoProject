1713cfc669b67df6b23bd4f1fe6b1e62be83ede9
Hannahh99 <000972499@mohawkcollege.ca>
2025-12-11 02:22:41 -0500

    - Add new scene - Update Duplicate for new scene

--- plinko-unity/Assets/Scripts/Duplicate.cs
+++ plinko-unity/Assets/Scripts/Duplicate.cs
@@ -6,16 +6,15 @@ public class Duplicate : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Disc") && collision.gameObject.name == "Disc")
+        if (collision.gameObject.CompareTag("Disc") && collision.gameObject.name == "Disc(Clone)")
         {
-            DuplicateDisc();
+            DuplicateDisc(collision.gameObject);
         }
     }
-
-    void DuplicateDisc()
+        void DuplicateDisc(GameObject disc)
     {
         Vector3 position = transform.position + new Vector3(1f, 3f, 0); // Offset
-        Quaternion rotation = transform.rotation;
+        Quaternion rotation = disc.transform.rotation;
         GameObject discCopy = Instantiate(disc, position, rotation);
     }
 }
